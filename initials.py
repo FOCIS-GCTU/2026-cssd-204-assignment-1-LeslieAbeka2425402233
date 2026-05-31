b3366df0d8ad2362aa8cab8a61c942d8cfce7852
LeslieAbeka2425402233 <2425402233@live.gctu.edu.gh>
2026-05-31 20:25:02 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+    # File: initials.py
+    # Description: Print out my initials.
+    # Assignment Number: 1
+    #
+    # Name: Leslie Abeka Ashirifi
+    # STUDENT ID:  2425402233
+    # Email: 2425402233@live.gctu.edu.gh
+    # Grader: Augustus Buckman
+    #
+    # On my honor, Leslie Abeka Ashirifi, this programming assignment is my own work
+    # and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
+    def main():
+        # Print initials LAA in small caps and large stylized block letters using print and strings only
+
+        print()
+        print("...LAA")
+        print()
+        print("...LL          ...  ...    AAAA    ...  ...    AAAA    ...")
+        print("...LL          ...  ...   AA  AA   ...  ...   AA  AA   ...")
+        print("...LL          ...  ...  AA    AA  ...  ...  AA    AA  ...")
+        print("...LL          ...  ... AA      AA ...  ... AA      AA ...")
+        print("...LL          ...  ...AA        AA...  ...AA        AA...")
+        print("...LL          ...  ...AAAAAAAAAAAA...  ...AAAAAAAAAAAA...")
+        print("...LL          ...  ...AA        AA...  ...AA        AA...")
+        print("...LL          ...  ...AA        AA...  ...AA        AA...")
+        print("...LL          ...**...AA        AA...**...AA        AA...**")
+        print("...LLLLLLLLLLLL...**...AA        AA...**...AA        AA...**")
+        print()
 
 
 
-main()
+    main()
